5af66b3415119de2c50e2f34557163d182300519
Froler27 <wqpyihu@163.com>
2022-01-24 14:54:57 +0800

    初版

--- Data/Shaders/frag.glsl
+++ Data/Shaders/frag.glsl
@@ -1,9 +1,18 @@
 #version 430
 
+in vec3 normal_v;
 in vec4 color_v;
+in vec2 texCoord_v;
+
 out vec4 color_f;
 
-void main(void)
+uniform sampler2D texture0;
+
+void main(void) 
 {
-	color_f = color_v;
+	if (texCoord_v.x < 0.5) {
+		color_f = texture(texture0, texCoord_v);
+	} else { 
+		color_f = color_v;
+	}
 }
--- Data/Shaders/vert.glsl
+++ Data/Shaders/vert.glsl
@@ -1,16 +1,24 @@
 #version 430
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec4 color;
+layout (location = 3) in vec2 texCoord;
 
+out vec3 normal_v;
 out vec4 color_v;
+out vec2 texCoord_v;
 
 uniform mat4 world_mat_u;
 uniform mat4 view_mat_u;
 uniform mat4 proj_mat_u;
 //uniform vec4 color_u;
 
+
 void main(void)
 {
 	gl_Position = proj_mat_u * view_mat_u * world_mat_u * vec4(position, 1.0);
-	color_v = vec4(1,0,0,1);
+	normal_v = normal;
+	color_v = color;
+	texCoord_v = texCoord;
 }
